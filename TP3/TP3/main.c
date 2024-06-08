4d2f2019f477fe954288f8d48b93d59f9b3d9827
axeltantalean1 <axel.tantalean@alu.ing.unlp.edu.ar>
2024-06-08 18:06:25 -0300

    Formateo y muestra de los datos en la terminal Presionando 3 veces s se detiene el programa Falta poner que se vuelva a reanudar

--- TP3/TP3/main.c
+++ TP3/TP3/main.c
@@ -6,35 +6,66 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #define F_CPU 16000000UL
 #include <util/delay.h>
 #include "dht11.h"
-#include "lcd.h"
+#define BR9600 (0x67)	// 0x67=103 configura BAUDRATE=9600 a 16MHz
 
+volatile static uint8_t contador=0;
+volatile uint8_t RX_Buffer=0;		// Variables que se modificara cuando se atienda la interrupcion
+
+uint8_t* convNumaCar(uint8_t);
 
 int main(void)
-{
-    /* Replace with your application code */
+{	
 	
-	LCDinit();
-	_delay_ms(100);
-	LCDhome();
-    
+    SerialPort_Init(BR9600); 		// Inicializo formato 8N1 y BAUDRATE = 9600bps
+    SerialPort_TX_Enable();			// Activo el Transmisor del Puerto Serie (1<<TXEN0) en UCSR0B
+    SerialPort_RX_Enable();			// Activo el Receptor del Puerto Serie	(1<<RXEN0) en UCSR0B
+	SerialPort_RX_Interrupt_Enable();	// Activo Interrupción de recepcion.
+	SerialPort_Send_String("Medicion de temperatura y humedad. Presionando 3 veces s o S se finaliza el programa de medicion.\n");   // Envío el mensaje de Bienvenida
+
+	sei();
+	
 	uint8_t h_t[4]={0,0,0,0};
+	uint8_t cadenaT[3],cadenaH[3];
 	uint8_t flagCheck=0;
+	uint8_t msg[100];
 	while (1) 
     {
 		flagCheck=leer(h_t);
+	
 		if(flagCheck){
-			LCDclr();
-			LCDGotoXY(0,0);
-			LCDescribeDato(h_t[0],2);
-			LCDGotoXY(0,1);
-			LCDescribeDato(h_t[2],2);
+			sprintf(cadenaT, "%d", h_t[2]);			//transformo los enteros a cadena de caracteres
+			sprintf(cadenaH, "%d", h_t[0]);			
+			strcpy(msg, "TEMP: ");					// uso strcpy para iniciar el mensaje
+			strcat(msg, cadenaT);
+			strcat(msg, "°C HUM: ");
+			strcat(msg, cadenaH);
+			strcat(msg, "% FECHA: - HORA: -\r\n");	//despues se va a tener que poner la fecha y hora con el otro periferico q usa i2c
+			SerialPort_Send_String(msg);			//mando el mensaje a la terminal
 		}
+		else
+			SerialPort_Send_String("Error en la transmision. (Fallo el Checksum)\r\n");
+		
+		
+		if(contador == 3){
+			SerialPort_Send_String("Se finalizó la medicion. Adios :) \r\n");  // Envío el string de despedida
+			while(1);
+		}
+
 		_delay_ms(2000);
 	}
+}
+ISR(USART_RX_vect){
+	RX_Buffer = UDR0; //la lectura del UDR borra flag RXC
+	if((RX_Buffer == 's') || (RX_Buffer == 'S')){
+		contador++;
+	}
+		
 }
 
